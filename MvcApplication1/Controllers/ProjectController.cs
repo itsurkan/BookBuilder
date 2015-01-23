84ebc4761726d98c89c7b7b867548945ef7450a2
Ivan Tsurkan <tsurkan_i@mail.ru>
2015-01-23 13:21:21 +0200

    new project, edit, delete

--- MvcApplication1/Controllers/ProjectController.cs
+++ MvcApplication1/Controllers/ProjectController.cs
@@ -9,6 +9,8 @@ using System.Web.Mvc;
 using System.Xml.Linq;
 using MvcApplication1.Classes;
 using MvcApplication1.DbModels;
+using MvcApplication1.Filters;
+using MvcApplication1.Resources;
 using  PagedList;
 using PagedList.Mvc;
 
@@ -17,6 +19,8 @@ namespace MvcApplication1.Controllers
     public class ProjectController : Controller
     {
         public Entities RepoContext = MvcApplication.db;
+        public NLog.Logger Log = MvcApplication.logger;
+        public static string PathToFiles = "App_Data/";
 
         //
         // GET: /Projects/
@@ -39,7 +43,7 @@ namespace MvcApplication1.Controllers
 
             ViewBag.CurrentFilter = searchString;
 
-            var projects = from s in RepoContext.Projects where s.UserLogin == "root" select s;
+            var projects = from s in RepoContext.Projects where s.UserLogin == WebSecurity.UserLogin select s;
             if (!String.IsNullOrEmpty(searchString))
             {
                 projects = projects.Where(s => s.Title.Contains(searchString)
@@ -73,25 +77,33 @@ namespace MvcApplication1.Controllers
             return View();
         }
 
-        [HttpPost, ActionName("NewProject")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Title, Description")]Projects projects)
+        public ActionResult NewProject([Bind(Include = "Title, Description")]Projects project)
         {
-            //try
-            //{
-            //    if (ModelState.IsValid)
-            //    {
-            //        db.Students.Add(student);
-            //        db.SaveChanges();
-            //        return RedirectToAction("Index");
-            //    }
-            //}
-            //catch (RetryLimitExceededException /* dex */)
-            //{
-            //    //Log the error (uncomment dex variable name and add a line here to write a log.
-            //    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
-            //}
-            return View();
+            Log.Info("new project {0}", DateTime.Now);
+            project.Date = DateTime.Now;
+            project.UserLogin = WebSecurity.UserLogin;
+            project.Path = project.Title.Translit();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    RepoContext.Projects.Add(project);
+                    RepoContext.SaveChanges();
+
+                    Log.Info("new project success {0}", DateTime.Now);
+                    return RedirectToAction("ProjectsList");
+                }
+            }
+            catch (Exception /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", Messages.Unable_to_save_changes);
+
+                Log.Info("new project error {0}", DateTime.Now);
+            }
+            return View(project);
         }
 
         public ActionResult Setting()
@@ -100,34 +112,11 @@ namespace MvcApplication1.Controllers
 
             return View();
         }
-
-
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete()
-        {
-            MvcApplication.logger.Info("Try to gelete {0}", DateTime.Now.TimeOfDay);
-          
-            //try
-            //{
-            //    Projects project =  (from p in RepoContext.Projects where p.Id == id select p).First();
-            //    //db.Projects.Remove();
-            //    RepoContext.SubmitChanges();
-            //}
-            //catch (Exception/* dex */)
-            //{
-            //    MvcApplication.logger.Info("Try to gelete exception {0}", DateTime.Now.TimeOfDay);
-            //    //Log the error (uncomment dex variable name and add a line here to write a log.
-            //    return RedirectToAction("Delete", new { id = id, saveChangesError = true });
-            //}
-            return RedirectToAction("ProjectsList", "Project");
-        }
-
+  
         [HttpGet]
         public ActionResult Edit(int? id)
         {
-            MvcApplication.logger.Info("Edit title page {0}", DateTime.Now);
+            Log.Info("Edit title page {0}", DateTime.Now);
 
             if (id == null)
             {
@@ -141,7 +130,7 @@ namespace MvcApplication1.Controllers
                 return HttpNotFound();
             }
 
-            MvcApplication.logger.Info("Edit title page exit {0}", DateTime.Now);
+            Log.Info("Edit title page exit {0}", DateTime.Now);
 
             return View(project);
 
@@ -158,7 +147,7 @@ namespace MvcApplication1.Controllers
             }
             var projecttoUpdate = RepoContext.Projects.Find(id);
             if (TryUpdateModel(projecttoUpdate, "",
-               new string[] { "Title", "Descriprion" }))
+               new string[] { "Title", "Description" }))
             {
                 try
                 {
@@ -178,11 +167,63 @@ namespace MvcApplication1.Controllers
                 MvcApplication.logger.Info("Edit title page end{0}", DateTime.Now);
 
             }
-            return View(projecttoUpdate);
+             return View(projecttoUpdate);
         }
 
+        [HttpGet]
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+            }
+            Projects project = RepoContext.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
 
+        // POST: Student/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int? id)
+        {
+            try
+            {
+                MvcApplication.logger.Info("Delete project {0}", DateTime.Now);
 
+                Projects project= RepoContext.Projects.Find(id);
+                RepoContext.Projects.Remove(project);
+                RepoContext.SaveChanges();
+            }
+            catch (Exception/* dex */)
+            {
+                MvcApplication.logger.Info("Delete project {0} exception {1}", id, DateTime.Now);
 
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Setting", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("ProjectsList");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            //if (disposing)
+            //{
+            //    RepoContext.Dispose();
+            //}
+            //base.Dispose(disposing);
+        }
+
+        [HttpPost]
+        public ActionResult ArchiveProject(object value)
+        {
+            return RedirectToAction("ProjectsList");
+        }
     }
 }
